1a3a2dac8582824b43685ca37b7bb104c9b39b9a
SungHeeCheol <hhhc0213@naver.com>
2022-11-19 18:26:47 +0900

    탭 기능구현, footer 완성

--- src/main/resources/static/js/main-tap.js
+++ src/main/resources/static/js/main-tap.js
@@ -1,3 +1,31 @@
-let tabList = document.querySelectorAll(".tab-area li");
-let tab = document.querySelectorAll(".tap");
+/*
+링크 클릭 시 클릭한 요소의 href속성의 값을 변수 tabTarget에 저장
+저장된 값에서 #을 삭제한다.
+(예시)tabTarget = #tabs-1에서 #을 삭제
+document.getElementById(tabs-1).style.visibility = "visible";
+*/
+let originaTarget = "#tabs-1"; //#을 없애려면 a.replace("b", "c");
+let targetLinks = document.querySelectorAll(".tab-area a");
+let tabContents = document.querySelectorAll(".tab > div");
+
+for(let i = 0; i < targetLinks.length; i++){
+    targetLinks[i].addEventListener('click', function(e){
+        let originaTarget = e.target.getAttribute("href");
+        let tabTarget = originaTarget.replace("#", "");
+
+        for(let j = 0; j < tabContents.length; j++){
+            tabContents[j].style.visibility = "hidden";
+        }
+
+        document.getElementById(tabTarget).style.visibility = "visible";
+
+        for(let j = 0; j < targetLinks.length; j++){
+            targetLinks[j].classList.remove("active");
+            e.target.classList.add("active");
+        }
+
+    });
+}
+document.getElementById("tabs-1").style.visibility = "visible";
+
 
