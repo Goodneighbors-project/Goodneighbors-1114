13a20ca4256375a7ef78156abc0a1303fd25825c
Choiyeungchan <dudcks3782@naver.com>
2022-11-25 15:16:14 +0900

    no message

--- src/main/resources/static/js/account/login.js
+++ src/main/resources/static/js/account/login.js
@@ -1,26 +1,26 @@
-//
-//class KakaoLogin {
-//
-//     getkakao () {
-//          $.ajax({
-//               url: '/login/getKakaoAuthUrl',
-//               type: 'get',
-//               async: false,
-//               dataType: 'text',
-//               success: function (res) {
-//                    location.href = res
-//               }
-//          });
-//
-//     }
-//
-//     $(document).ready(function() {
-//          let kakaoInfo = '${kakaoInfo}';
-//
-//          if(kakaoInfo != "") {
-//               let data = JSON.parse(kakaoInfo);
-//          }
-//     });
-//
-//}
+
+class KakaoLogin {
+
+     getkakao () {
+          $.ajax({
+               url: '/login/getKakaoAuthUrl',
+               type: 'get',
+               async: false,
+               dataType: 'text',
+               success: function (res) {
+                    location.href = res
+               }
+          });
+          
+     }
+
+     $(document).ready(function() {
+          let kakaoInfo = '${kakaoInfo}';
+
+          if(kakaoInfo != "") {
+               let data = JSON.parse(kakaoInfo);
+          }
+     });
+
+}
 
