8296750251f8519a911bcf54cd0137b2a17f5bb4
SHC <hhhc0213@naver.com>
2022-11-25 14:43:41 +0900

    no message

--- src/main/resources/static/js/account/resgister.js
+++ src/main/resources/static/js/account/resgister.js
@@ -1,56 +1,3 @@
-const btnCancel = document.querySelector(".btn-cancel");
-
-btnCancel.onclick = () => {
-    location.href = "/account/login"
-}
-
-const btnJoin = document.querySelector(".btn-join");
-
-btnJoin.onclick = () => {
-    location.href = "/account/complete"
-}
-
-
-
-class SearchPostcode {
-    static #instance = null;
-
-    static getInstance() {
-        if(this.#instance == null) {
-            this.#instance = new SearchPostcode();
-        }
-        return this.#instance;
-    }
-
-    #daumApi = null;
-    constructor() {
-        this.#daumApi = new daum.Postcode({
-            oncomplete: function(data) {
-                var addr = '';
-        
-                if (data.userSelectedType === 'R') {
-                    addr = data.roadAddress;
-                } else {
-                    addr = data.jibunAddress;
-                }
-                
-                document.getElementById("zonecode").value = data.zonecode;
-                document.getElementById("address").value = addr;
-        
-                document.getElementById("addressSub").focus();
-            }
-          })
-        this.addAddressButtonEvent();
-    }
-
-    setAddressInfo(data) {
-
-    }
-
-    addAddressButtonEvent() {
-        document.querySelector(".btn-postsearch").onclick = () => {
-            this.#daumApi.open();
-=======
 class Option{
 
     static #instance = null;
@@ -193,12 +140,9 @@ class InputData{
                     ErrorMessage.getInstance().loadErrorMessage(error.responseJSON.data);
                 }
             });
+
         }
     }
-}
-
-window.onload = () => {
-    SearchPostcode.getInstance();
 }
 
 class ErrorMessage{
@@ -319,4 +263,7 @@ window.onload = () => {
     InputData.getInstance().getRegisterApi();
 
 }
+
+
+
 
