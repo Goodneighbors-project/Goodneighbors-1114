1576e5ab8e2d9f971e6a94abf2c116a38ee12a96
SHC <hhhc0213@naver.com>
2022-11-25 14:18:01 +0900

    no message

--- src/main/java/com/project/goodneighbors20221114/config/SecurityConfig.java
+++ src/main/java/com/project/goodneighbors20221114/config/SecurityConfig.java
@@ -1,13 +1,17 @@
 package com.project.goodneighbors20221114.config;
 
+<<<<<<< HEAD
 import com.project.goodneighbors20221114.security.AuthFailureHandler;
 import com.project.goodneighbors20221114.service.PrincipalOauth2Service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
+=======
+>>>>>>> SHC
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+<<<<<<< HEAD
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
@@ -27,11 +31,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic().disable();
         http.authorizeRequests()
                 .antMatchers("/mypage/index")
+=======
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.authorizeRequests()
+                .antMatchers("/account/mypage", "/index")
+>>>>>>> SHC
                 .authenticated()
                 .anyRequest()
                 .permitAll()
                 .and()
                 .formLogin()
+<<<<<<< HEAD
                 .usernameParameter("username")
                 .loginPage("/account/login")
                 .loginProcessingUrl("/accouont/login")
@@ -42,5 +59,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .userService(principalOauth2Service)
                 .and()
                 .defaultSuccessUrl("/main");
+=======
+                .loginPage("/account/login")
+                .defaultSuccessUrl("/main");
+
+>>>>>>> SHC
     }
 }
