f384ffb7c4fcbeb2a44eaa05275e3f6739939b5c
SHC <hhhc0213@naver.com>
2022-11-25 14:41:41 +0900

    no message

--- src/main/java/com/project/goodneighbors20221114/security/PrincipalDetails.java
+++ src/main/java/com/project/goodneighbors20221114/security/PrincipalDetails.java
@@ -41,7 +41,7 @@ public class PrincipalDetails implements UserDetails, OAuth2User {
 
     @Override
     public String getUsername() {
-        return user.getUserName();
+        return user.getUsername();
     }
 
     @Override
