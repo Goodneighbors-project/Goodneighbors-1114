06b7a8e96e94ce4a1df8b70daae61728ddb220d6
SHC <hhhc0213@naver.com>
2022-11-30 16:13:33 +0900

    이미지 저장 변경

--- src/main/java/com/project/goodneighbors20221114/service/admin/DonationManagementServiceImpl.java
+++ src/main/java/com/project/goodneighbors20221114/service/admin/DonationManagementServiceImpl.java
@@ -55,13 +55,76 @@ public class DonationManagementServiceImpl implements DonationManagementService{
             Map<String, String> errorMap = new HashMap<String, String>();
             errorMap.put("register", "빈칸을 허용하지 않습니다.");
             throw new CustomValidationException("사진을 제외하고는 빈칸을 허용하지 않습니다.", errorMap);
-
-
-
         }
-
-
     }
 
+    private List<DonationImg> getDonationImgFiles(List<MultipartFile> files, int donationId){
+
+        List<DonationImg> donationImgFiles = new ArrayList<DonationImg>();
+
+        files.forEach(file -> {
+
+            Resource resource = resourceLoader.getResource("classpath:static/upload/donation");
+            String filePath = null;
+
+            try {
+                if(!resource.exists()){
+
+                    String tempPath = resourceLoader.getResource("classpath:static").getURI().toString();
+                    tempPath = tempPath.substring(tempPath.indexOf("/") + 1);
+
+                    File f = new File(tempPath + "/upload/donation");
+                    f.mkdirs();
+                }
+
+                filePath = resource.getURI().toString();
+                filePath = filePath.substring(filePath.indexOf("/") + 1);
+            } catch (IOException e) {
+
+                throw new RuntimeException(e);
+            }
+
+            String origin_name = file.getOriginalFilename();
+            String extension = origin_name.substring(origin_name.lastIndexOf("."));
+            String save_name = UUID.randomUUID().toString().replaceAll("-", "") + extension;
+
+            Path path = Paths.get(filePath + "/" + save_name);
+
+            File f = new File(filePath + "/donation");
+
+            try {
+
+                Files.write(path, file.getBytes());
+            } catch (IOException e) {
+
+                throw new CustomInternalServerErrorException(e.getMessage());
+            }
+
+            DonationImg donationImg = DonationImg.builder()
+                    .donation_id(donationId)
+                    .origin_name(origin_name)
+                    .save_name(save_name)
+                    .build();
+
+            donationImgFiles.add(donationImg);
+        });
+
+        return donationImgFiles;
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
